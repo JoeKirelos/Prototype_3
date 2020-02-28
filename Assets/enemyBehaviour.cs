dc50ead0f55c82cc656773b725e61a8fe35dd559
LastEndConductor <42978769+LastEndConductor@users.noreply.github.com>
2020-02-28 17:19:47 -0500

    reupload of p3

---
+++ Assets/enemyBehaviour.cs
@@ -0,0 +1,107 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class enemyBehaviour : MonoBehaviour
+{
+    public Animator animator;
+    public int health = 3;
+    public Transform dangerZone;
+    public float dangerRange = 0.5f;
+    public LayerMask playerLayer;
+    public bool alreadyOnGuard = false;
+    public Collider2D playerPresent;
+    public int damage = 1;
+    public bool cooldown = true;
+    public int attackCounter = 0;
+    void Start()
+    {
+        AttackInitiator();
+        IsOnGuard();
+    }
+
+    private void Update()
+    {
+        playerPresent = Physics2D.OverlapCircle(dangerZone.position, dangerRange, playerLayer);
+        animator.SetBool("Danger", alreadyOnGuard);
+    }
+    public void IsOnGuard()
+    {
+        if (playerPresent != null)
+        {
+            if (!alreadyOnGuard)
+            {
+                alreadyOnGuard = true;
+            }
+        }
+        Invoke("IsOnGuard", 2f);
+    }
+    public void TakeDamage(int damage)
+    {
+        health -= damage;
+        animator.SetTrigger("Hurting");
+        if (health <= 0)
+        {
+            Die();
+        }
+    }
+    void Die()
+    {
+        animator.SetBool("Died", true);
+        Invoke("ReloadScene", 0.75f);
+    }
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.DrawWireSphere(dangerZone.position, dangerRange);
+    }
+
+    public void AttackInitiator()
+    {
+        //int randomizer = Random.Range(500, 1500);
+        if(playerPresent != null)
+        {
+            if (attackCounter == 0)
+            {
+                cooldown = false;
+                animator.SetTrigger("Swing");
+            }
+        }
+        Invoke("AttackInitiator", 1.75f);
+        //Invoke("AttackInitiator", 0.002f * randomizer);
+    }
+    public void DamagePlayer()
+    {
+        if (playerPresent != null)
+        {
+            bool parried = playerPresent.gameObject.GetComponent<playermovement>().parry;
+            if (parried == true)
+            {
+                alreadyOnGuard = false;
+            }
+            if (!parried)
+            {
+                bool blocked = playerPresent.gameObject.GetComponent<playermovement>().guard;
+                if (blocked)
+                {
+                    playerPresent.gameObject.GetComponent<playermovement>().DamageStamina(damage);
+                }
+                else if (!blocked)
+                {
+                   
+                    playerPresent.gameObject.GetComponent<playermovement>().DamageHealth(damage);
+
+                }
+
+            }
+        }
+    }
+    public void Cooldown()
+    {
+        cooldown = true;
+    }
+    void ReloadScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
---
+++ Assets/playermovement.cs
@@ -0,0 +1,157 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class playermovement : MonoBehaviour
+{
+    public Vector3 move = new Vector3(1f, 0f, 0f);
+    public Animator animator;
+    public float horizontalDir = 0f;
+    public float mS = 1.5f;
+    public GameObject player;
+    public Transform attackPoint;
+    public float attackRange = 0.5f;
+    public LayerMask enemyLayers;
+    public int attackDamage = 1;
+    public bool parry = false;
+    public bool stance = false;
+    public int stamina = 3 ;
+    public int health = 3;
+    public bool guard = false;
+    public bool attacking = false;
+    public bool died;
+    Collider2D enemy;
+    private void Start()
+    {
+        
+    }
+    void Update()
+    {
+        horizontalDir = Input.GetAxisRaw("Horizontal") * mS;
+        transform.position += move* horizontalDir * Time.deltaTime * mS;
+        animator.SetFloat("Speed", Mathf.Abs(horizontalDir));
+        if(horizontalDir < 0 && !stance)
+        {
+            Vector3 side = player.transform.localScale;
+            side.x = -1;
+            player.transform.localScale = side;
+        } else if(horizontalDir > 0)
+        {
+            Vector3 side = player.transform.localScale;
+            side.x = 1;
+            player.transform.localScale = side;
+        }
+        Guard();
+        if(Input.GetKeyDown("m") && stance)
+        {
+            Attack();
+        }
+        OnGuard();
+        Deading();
+    }
+
+    void Guard()
+    {
+        if (Input.GetKeyDown("w") && stamina >0)
+        {
+            mS = 0.5f;
+            stance = true;
+            animator.SetBool("Combat", stance);
+        }
+        else if (Input.GetKeyUp("w"))
+        {
+            mS = 1.5f;
+            stance = false;
+            animator.SetBool("Combat", stance);
+        }
+       
+    }
+    void Attack()
+    {
+        animator.SetTrigger("Attack");
+        enemy =  Physics2D.OverlapCircle(attackPoint.position,attackRange, enemyLayers);
+
+    }
+    public void CalcEnemyDmg()
+    {
+        if (enemy != null)
+        {
+            bool failedParry = enemy.GetComponent<enemyBehaviour>().alreadyOnGuard;
+            if (!failedParry)
+            {
+                enemy.GetComponent<enemyBehaviour>().TakeDamage(attackDamage);
+            }
+        }
+    }
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.DrawWireSphere(attackPoint.position, attackRange);
+    }
+
+    void Parrying()
+    {
+        parry = true;
+    }
+    void NotParrying()
+    {
+        parry = false;
+    }
+    void AttackAnimation()
+    {
+        attacking = true;
+    }
+    void AttackAnimationEnded()
+    {
+        attacking = false;
+    }
+    void OnGuard()
+    {
+        if(stance & !attacking)
+        {
+            guard = true;
+        }
+        else
+        {
+            guard = false;
+        }
+        
+    }
+    public void DamageStamina(int damage)
+    {
+        stamina -= damage;
+        if (stamina <=0)
+        {
+            stance = false;
+            animator.SetBool("Combat", stance);
+            Invoke("StamRegen", 3);
+        }
+    }
+    public void DamageHealth(int damage)
+    {
+        animator.SetTrigger("Hurt");
+        health-= damage;
+    }
+    public void StamRegen()
+    {
+        stamina = 5;
+    }
+
+    void Deading()
+    {
+        if (health <= 0)
+        {
+            died = true;
+        }
+        animator.SetBool("DED", died);
+        if(died == true)
+        {
+            Invoke("ReloadScene", 0.75f);
+        }
+    }
+
+    void ReloadScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
